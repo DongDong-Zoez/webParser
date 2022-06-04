efa0e3b73fa114c6c6750c03f8d2223428eeea0c
DongDong <82877840+DongDong-Zoez@users.noreply.github.com>
2022-06-04 15:49:38 +0800

    Update demo.py

--- ptt/demo.py
+++ ptt/demo.py
@@ -1,11 +1,15 @@
 import argparse
 from ptt import PTT
+import os
+from pathlib import Path
+
+ROOT = str(Path(__file__).parent) + '/res/'
 
 def parse_args():
     """
     -k: keyword
     -f: forum
-    -p: path
+    -p: pasth
     -n: pages
     -a: author
     -l: headers
@@ -19,19 +23,20 @@ def parse_args():
                                                                      
     parser.add_argument('-k', help='your searching keyword', default=None)
     parser.add_argument('-f', help='The forum of ptt', type=str, default='Stock')
-    parser.add_argument('-p', help='path to save your txt logger file', type=str, default='./res/logger.txt')
+    parser.add_argument('-p', help='path to save your txt logger file', type=str, default='logger.txt')
     parser.add_argument('-n', help='number of page to parse', type=int, default=2)
     parser.add_argument('-a', help='search article from specify author', default=None)
     parser.add_argument('-l', help='the headers of requests module', type=dict, default={})
     parser.add_argument('-c', help='write output to csv', type=bool, default=False)
     parser.add_argument('-x', help='write output to xlsx', type=bool, default=True)
-    parser.add_argument('-cp', help='path to save your csv file', default='./res/ptt.csv')
-    parser.add_argument('-xp', help='path to save your xlsx file', default='./res/ptt.xlsx')
+    parser.add_argument('-cp', help='path to save your csv file', default='ptt.csv')
+    parser.add_argument('-xp', help='path to save your xlsx file', default='ptt.xlsx')
     
     args = parser.parse_args()                                        
     return args
 
 if __name__ == '__main__':
+
     args = parse_args()
 
     params = {}
@@ -43,11 +48,11 @@ if __name__ == '__main__':
         num_pages=args.n,
         headers=args.l,
         params=params,
-        txt_path=args.p
+        txt_path=ROOT+args.p
         )
         
     ptt.fetch()
     if args.c:
-        ptt.to_csv(args.cp)
+        ptt.to_csv(ROOT+args.cp)
     if args.x: 
-        ptt.to_xlsx(args.xp)
+        ptt.to_xlsx(ROOT+args.xp)
